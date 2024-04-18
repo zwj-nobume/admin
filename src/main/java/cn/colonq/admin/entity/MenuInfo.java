b7d237446dd0aaf4441eaef0fd27d3d7e13e5143
nobume <zwj-nobume@outlook.com>
2024-04-18 08:57:26 +0800

    菜单字段修改

--- src/main/java/cn/colonq/admin/entity/MenuInfo.java
+++ src/main/java/cn/colonq/admin/entity/MenuInfo.java
@@ -8,10 +8,11 @@ import cn.colonq.admin.config.CompEnum;
 
 @Table(tableName = "menu_info", idName = "menu_id")
 public record MenuInfo(
-        @TableField(query = false, isInsert = false) String menuId,
-        @TableField(comp = CompEnum.like) String menuName,
-        @TableField(comp = CompEnum.like) String menuLabel,
-        String parentId,
-        @TableField(comp = CompEnum.like, isUpdate = false) String createName,
-        @TableField(comp = CompEnum.ge, isInsert = false, isUpdate = false) Date createTime) {
+		@TableField(query = false, isInsert = false) String menuId,
+		@TableField(comp = CompEnum.like) String menuName,
+		@TableField(comp = CompEnum.like) String menuLabel,
+		String permission,
+		String parentId,
+		@TableField(comp = CompEnum.like, isUpdate = false) String createName,
+		@TableField(comp = CompEnum.ge, isInsert = false, isUpdate = false) Date createTime) {
 }
