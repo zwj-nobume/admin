79f8f5d14edbf8d941749a5f340e57cdf8c83569
nobume <zwj-nobume@outlook.com>
2024-05-25 22:39:30 +0800

    异常返回值修改

--- src/main/java/cn/colonq/admin/config/GlobalExceptionHandler.java
+++ src/main/java/cn/colonq/admin/config/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import cn.colonq.admin.entity.Result;
@@ -17,52 +16,59 @@ import jakarta.servlet.http.HttpServletResponse;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ResponseBody
 	@ExceptionHandler(NoResourceFoundException.class)
-	public Result handleNoResourceFound(NoResourceFoundException e) {
-		return Result.notFound();
+	public void handleNoResourceFound(NoResourceFoundException e, HttpServletResponse res) {
+		sendError(Result.notFound(), res);
 	}
 
-	@ResponseBody
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Result handleValidated(MethodArgumentNotValidException e) {
+	public void handleValidated(MethodArgumentNotValidException e, HttpServletResponse res) {
 		final String message = e.getMessage();
-		return Result.error(message.substring(message.lastIndexOf("default message"), message.length() - 2));
+		sendError(Result.error(message.substring(message.lastIndexOf("default message"), message.length() - 2)), res);
 	}
 
-	@ResponseBody
 	@ExceptionHandler(DuplicateKeyException.class)
-	public Result handleDuplicateKey(DuplicateKeyException e) {
+	public void handleDuplicateKey(DuplicateKeyException e, HttpServletResponse res) {
 		// TODO 入日志库
-		return Result.error("数据唯一值重复错误");
+		sendError(Result.error("数据唯一值重复错误"), res);
 	}
 
-	@ResponseBody
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-	public Result handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+	public void handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e,
+			HttpServletResponse res) {
 		// TODO 入日志库
-		return Result.error("请求类型错误, GET、PUT、POST、DELETE");
+		sendError(Result.error("请求类型错误, GET、PUT、POST、DELETE"), res);
 	}
 
-	@ResponseBody
 	@ExceptionHandler(ServiceException.class)
-	public Result handleServiceError(ServiceException e) {
+	public void handleServiceError(ServiceException e, HttpServletResponse res) {
 		// TODO 入日志库
-		return Result.error(e);
+		sendError(Result.error(e), res);
 	}
 
-	@ResponseBody
 	@ExceptionHandler(HttpMessageNotReadableException.class)
-	public Result handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
+	public void handleHttpMessageNotReadable(HttpMessageNotReadableException e, HttpServletResponse res) {
 		// TODO 入日志库
-		return Result.error("无法解析请求Body");
+		sendError(Result.error("无法解析请求Body"), res);
 	}
 
-	public void filterServiceError(ServiceException e, HttpServletResponse response) {
+	public void filterServiceError(ServiceException err, HttpServletResponse res) {
+		sendError(err, res);
+	}
+
+	private void sendError(Result result, HttpServletResponse res) {
 		try {
-			response.sendError(e.getStatusCode().value(), e.getMsg());
-		} catch (IOException e1) {
-			e1.printStackTrace();
+			res.sendError(result.status(), result.message());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void sendError(ServiceException err, HttpServletResponse res) {
+		try {
+			res.sendError(err.getStatusCode().value(), err.getMsg());
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
